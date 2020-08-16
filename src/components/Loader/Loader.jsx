a35fc669e61347776a3eaa3db599fa6450f66362
Christian Santos <christian.pinho@outlook.com>
2020-08-16 00:13:45 -0300

    creating loading component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+import { ReactComponent as LoaderLogo } from '../../assets/icon/loader.svg';
+
+const loaderSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const LoaderStyle = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 70vh;
+
+  .loader-logo {
+    animation: ${loaderSpin} infinite 2.5s linear;
+    height: 8rem;
+    fill: #fff;
+  }
+`;
+
+const Loader = () => (
+  <LoaderStyle data-testid="loader">
+    <LoaderLogo className="loader-logo" />
+  </LoaderStyle>
+);
+
+export default Loader;
