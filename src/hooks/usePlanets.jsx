b90b0c20053bcf9d88ca29bcb0fcf4e9c23e111c
Christian Santos <christian.pinho@outlook.com>
2020-08-16 00:14:37 -0300

    criando hook de paginação

---
+++ src/hooks/usePlanets.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+
+import { getAllPlanets } from '../services/api';
+
+export default function usePlanets() {
+  const [planets, setPlanets] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [actualPage, setActualPage] = useState(1);
+
+  useEffect(() => {
+    let mounted = true;
+    const fetchData = async () => {
+      const response = await getAllPlanets(actualPage);
+      if (mounted) {
+        setPlanets(response);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+    return () => {
+      mounted = false;
+    };
+  }, [actualPage]);
+
+  return {
+    loading,
+    planets,
+    actualPage,
+    setActualPage,
+  };
+}
